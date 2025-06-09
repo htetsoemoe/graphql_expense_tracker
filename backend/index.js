103a05fef29ff58a0dde28288c38aba9447aa651
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-06-09 17:16:32 +0630

    resolved req.body issue: need to downgrading express v5 to v4

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-import express from  'express'
+import express from 'express'
 import http from 'http'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './db/connectDB.js'
 
 import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 
 // mergedResolvers and mergedTypeDefs
@@ -19,32 +19,26 @@ const httpServer = http.createServer(app)
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
-    // plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
 
 // Ensure we wait for our server to start
-// await server.start()
+await server.start()
 
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
-// app.use(
-//     cors(
-//         "/graphql",
-//         cors({
-//             origin: "*",
-//             credentials: true,
-//         })
-//     ),
-//     express.json(),
-//     expressMiddleware(server, {
-//         context: async ({ req }) => ({ token: req.headers.token }),
-//     })
-// )
-
-const { url } = await startStandaloneServer(server)
-console.log(`🚀 Server ready at ${url}`)
+app.use(
+    "/",
+    cors(),
+    express.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ req }),
+    })
+)
 
 // Modified server startup
-// await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
-// await connectDB()
-// console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve))
+await connectDB()
+console.log(`🚀 Server ready at http://localhost:4000/`);
