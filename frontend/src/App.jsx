bdb4787d1fb51c83765ef0a9d4aa75cd9704ded0
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-06-16 12:03:23 +0630

    configured apollo client in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,15 @@ import SignIn from './pages/SignIn'
 import Transaction from './pages/Transaction'
 import NotFound from './pages/NotFound'
 import Header from './components/ui/Header'
+import { useQuery } from '@apollo/client'
+import { GET_AUTHENTICATED_USER } from './graphql/queries/user.query'
 
 const App = () => {
-  const authUser = false
+  const { loading, error, data } = useQuery(GET_AUTHENTICATED_USER)
 
   return (
     <div className='z-50 w-screen'> {/* add w-screen class */}
-      {authUser && <Header />}
+      {data?.authUser && <Header />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signin' element={<SignIn />} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,22 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import GridBackground from './components/ui/GridBackground.jsx'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  // TODO: Update the URI on production
+  uri: 'http://localhost:4000/graphql', // the URL of our GraphQL server.
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: 'include', // This tells Apollo Client to send cookies along with every request to the server.
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <GridBackground>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </GridBackground>
     </BrowserRouter>
   </StrictMode>,
