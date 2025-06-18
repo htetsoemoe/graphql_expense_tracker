9f5bfeff10803631a56cfa16d08da30b2c3a288b
htetsoemoe <soemoehtet.mdy@gmail.com>
2025-06-18 12:13:20 +0630

    feat(categoryStatisticData): when delete a specific transaction change data on Doughnut chart of home

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -23,7 +23,7 @@ const Card = ({ transaction, authUser }) => {
 
     // Handle delete transaction
     const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
-        refetchQueries: ["GetTransactions"],
+        refetchQueries: ["GetTransactions", "GetTransactionStatistics"],
     });
 
     // Capitalize the first letter of the description
